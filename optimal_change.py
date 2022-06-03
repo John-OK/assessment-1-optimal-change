dfbbb27e9461945e6334881e37a85dfbb843a19e
JohnOK <john.ok.esp@gmail.com>
2022-06-03 23:11:16 +0200

    Added more comments to clarify though process. Added test cases to spec file. Cleaned up comments.

--- optimal_change.py
+++ optimal_change.py
@@ -1,6 +1,6 @@
 from math import floor
 
-# map of denominations (in cents to avoid decimal errors)
+# Map of denominations (in cents to avoid decimal errors).
 denominations = {
     '$100'   : 10000,
     '$50'    : 5000,
@@ -14,14 +14,15 @@ denominations = {
     'penny'  : 1,
 }
 
-# order to cycle through when giving change
+# Order to cycle through when giving change.
 denom_order = ['$100', '$50', '$20',
                '$10', '$5', '$1', 'quarter', 'dime', 'nickel', 'penny']
 
-
+# Logic to format return text with proper punctuation and noun pluralization.
 def text_formatter(item_cost, amount_paid, denomination_map):
     change_message = f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is"
 
+    # Need these to separate from paper denominations (i.e., "bills")
     coins = ['quarter', 'dime', 'nickel', 'penny']
 
     for denom in denom_order:
@@ -40,17 +41,25 @@ def text_formatter(item_cost, amount_paid, denomination_map):
 
     change_message = change_message[:-1] + "."
     idx_last_comma = change_message.rfind(',')
-    change_message = change_message[0:idx_last_comma + 2] + "and " + change_message[idx_last_comma + 2:]
+    if idx_last_comma > 0:
+        change_message = change_message[0:idx_last_comma + 2] + "and " + change_message[idx_last_comma + 2:]
 
     return change_message
     
-
+# Main function to calulate proper optimal change and return properly formatted and grammatically correct string of optimal change to return.
 def optimal_change(item_cost, amount_paid):
+    if amount_paid < item_cost:
+        return "Insufficient payment."
+    elif item_cost == 0 or item_cost == amount_paid:
+        return "No change due."
+
+    # Convert to cents in order to avoid decimal errors
     change_due_in_cents = (amount_paid * 100) - (item_cost * 100)
 
     # dict of number of each denomination given in change
     return_denominations = {}
 
+    # Loop through denominations from highest to lowest to determine how many of each denomination to return and store result in "return_denominations" to be passed to "text_formatter."
     for denom in denom_order:
         value = denominations[denom]
         num_to_give = floor(change_due_in_cents / value)
@@ -60,6 +69,3 @@ def optimal_change(item_cost, amount_paid):
             change_due_in_cents = remaining_change_due
 
     return text_formatter(item_cost, amount_paid, return_denominations)
-
-
-print(optimal_change(31.51, 50))
